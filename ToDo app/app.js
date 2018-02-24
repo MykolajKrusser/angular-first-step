b8ca3f7b6669091bd0ceb2e82cafb5b8b6f69126
Mykolaj <32041427+MykolajKrusser@users.noreply.github.com>
2018-02-24 12:10:01 +0100

    v1.2 styles and code comments

--- ToDo app/app.js
+++ ToDo app/app.js
@@ -1,18 +1,18 @@
 'use strict';
-var app = angular.module("toDoApp", []);
-app.controller("ToDoCtrl", ToDoCtrl);
+var app = angular.module("toDoApp", []); //create angular app module
+app.controller("ToDoCtrl", ToDoCtrl); //create controller
 
-function ToDoCtrl() {
-    this.editMode = false;
-    this.todos = [];
-    this.addNewToDo = function () {
+function ToDoCtrl() { //function for controller
+    this.editMode = false; //for hide/show directive
+    this.todos = []; //array with li elements
+    this.addNewToDo = function () { //add function which add value of input modal to array
         this.todos.push(this.newToDo);
-        this.newToDo = "";
+        this.newToDo = ""; //clear input
     };
     this.triggerEditMode = function () {
-        this.editMode = !this.editMode;
+        this.editMode = !this.editMode; //change hide/show condition
     };
     this.deleteToDo = function (index) {
-        this.todos.splice(index, 1);
+        this.todos.splice(index, 1); //delete clicked li element from array defined by position index in html
     };
 }
